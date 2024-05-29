c66ae998efea4afc27bda267351d2d3d304b66a0
MuthuRahul <rahulpitchiah2004@gmail.com>
2024-05-29 12:04:36 +0530

    firstchanges

---
+++ react/sample/src/components/kandha.js
@@ -0,0 +1,14 @@
+function Login(){
+    return
+    (
+        <>
+        <h1>Kandasamy</h1>  
+        <h2>Naveen</h2>
+        <h3>Rahul</h3> 
+        <h4>Rahul</h4>
+        <h5>Kandasamy</h5>
+        </>
+        
+    )
+}
+export default Login;
---
+++ script.js
@@ -0,0 +1,44 @@
+const nav = document.querySelector(".nav"),
+ searchIcon = document.querySelector("#searchIcon"),
+ navOpenBtn = document.querySelector(".navOpenBtn"),
+ navCloseBtn = document.querySelector(".navCloseBtn");
+
+ searchIcon.addEventListener("click", () => {
+    nav.classList.toggle("openSearch");
+    nav.classList.remove("openNav");
+    if (nav.classList.contains("openSearch")) {
+        return searchIcon.classList.replace("uil-search", "uil-times");
+    }
+    searchIcon.classList.replace("uil-times", "uil-search");
+ });
+
+ navOpenBtn.addEventListener("click", () => {
+    nav.classList.add("openNav");
+    nav.classList.remove("openSearch");
+ });
+ navCloseBtn.addEventListener("click", () => {
+    nav.classList.remove("openNav");
+ });
+
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+   slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2.5,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
